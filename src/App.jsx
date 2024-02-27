0d9ce448d919a421ec5ff926465d93a5b354e6c1
Otto Hines <ottohines1@gmail.com>
2024-02-26 21:25:16 -0600

    correct refactor of fetching the BreedList using react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
--- src/fetchBreedList.js
+++ src/fetchBreedList.js
@@ -1,7 +1,16 @@
-const fetchBreedList = ({ queryKey }) => {
+const fetchBreedList = async ({ queryKey }) => {
   const animal = queryKey[1];
 
   if (!animal) return [];
+
+  const res = await fetch(`
+  http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
+
+  if (!res.ok) {
+    throw new Error(`Trouble fetching ${animal} breeds`);
+  }
+
+  return res.json();
 };
 
 export default fetchBreedList;
